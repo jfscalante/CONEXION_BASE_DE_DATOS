7e00f2fa70c3989b27589a65263b8d9d161e1dc7
jfscalante <scalante2002@gmail.com>
2023-11-18 16:37:51 -0500

    agregue unos estilos

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Formulario</title>
-	<link rel="stylesheet" type="text/css" href="estyle.css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
